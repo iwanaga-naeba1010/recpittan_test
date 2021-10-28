623846e34de933b5b0bca2c44afba5cf20153164
masahirookubo <stmhamachiii@gmail.com>
2021-10-29 01:00:39 +0900

    作成

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,32 @@ class HomeController < ApplicationController
   def index
     @categories = [
       { name: '音楽', description: "音楽療法\n和太鼓奏法など", image: 'https://recreation.sandbox.everyplus.jp/images/category/musuc.webp' },
-      { name: '健康', description: "音楽療法\n和太鼓奏法など", image: 'https://recreation.sandbox.everyplus.jp/images/category/musuc.webp' },
-      { name: '趣味', description: "音楽療法\n和太鼓奏法など", image: 'https://recreation.sandbox.everyplus.jp/images/category/musuc.webp' },
-      { name: '創作', description: "音楽療法\n和太鼓奏法など", image: 'https://recreation.sandbox.everyplus.jp/images/category/musuc.webp' },
-      { name: '旅行', description: "音楽療法\n和太鼓奏法など", image: 'https://recreation.sandbox.everyplus.jp/images/category/musuc.webp' },
-      { name: '飲食', description: "音楽療法\n和太鼓奏法など", image: 'https://recreation.sandbox.everyplus.jp/images/category/musuc.webp' },
-      { name: 'その他', description: "音楽療法\n和太鼓奏法など", image: 'https://recreation.sandbox.everyplus.jp/images/category/musuc.webp' },
+      { name: '健康', description: "音楽療法\n和太鼓奏法など", image: 'https://recreation.sandbox.everyplus.jp/images/category/health.webp' },
+      { name: '趣味', description: "音楽療法\n和太鼓奏法など", image: 'https://recreation.sandbox.everyplus.jp/images/category/hoby.webp' },
+      { name: '創作', description: "音楽療法\n和太鼓奏法など", image: 'https://recreation.sandbox.everyplus.jp/images/category/art.webp' },
+      { name: '旅行', description: "音楽療法\n和太鼓奏法など", image: 'https://recreation.sandbox.everyplus.jp/images/category/beauty.webp' },
+      { name: '飲食', description: "音楽療法\n和太鼓奏法など", image: 'https://recreation.sandbox.everyplus.jp/images/category/food.webp' },
+      { name: 'その他', description: "音楽療法\n和太鼓奏法など", image: 'https://recreation.sandbox.everyplus.jp/images/category/others.webp' },
+    ]
+    @yoshimoto_recs = [
+      {
+        title: '笑って健康！折りがとう！',
+        image: 'https://recreation.sandbox.everyplus.jp/resource/thumbnail/y/RGR24pRw69Y-320x180@1.webp',
+        tags: ['イベント', 'オンライン', '話題性あり'],
+        amount_time: 60,
+      },
+      {
+        title: '笑って健康！折りがとう！',
+        image: 'https://recreation.sandbox.everyplus.jp/resource/thumbnail/y/RGR24pRw69Y-320x180@1.webp',
+        tags: ['イベント', 'オンライン', '話題性あり'],
+        amount_time: 60,
+      },
+      {
+        title: '笑って健康！折りがとう！',
+        image: 'https://recreation.sandbox.everyplus.jp/resource/thumbnail/y/RGR24pRw69Y-320x180@1.webp',
+        tags: ['イベント', 'オンライン', '話題性あり'],
+        amount_time: 60,
+      },
     ]
   end
 end
