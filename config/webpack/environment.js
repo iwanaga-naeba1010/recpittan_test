24b649501cf977fe9c127bf1cbd49a2430ed3e26
masahirookubo <stmhamachiii@gmail.com>
2021-10-28 22:52:55 +0900

    bootstrapの追加

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,12 @@
 const { environment } = require('@rails/webpacker')
-
+// jQueryとBootstapのJSを使えるように設定
+const webpack = require('webpack')
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
+    Popper: 'popper.js'
+  })
+)
 module.exports = environment
