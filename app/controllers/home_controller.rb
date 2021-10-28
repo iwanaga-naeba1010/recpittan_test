4cc3a60b18a56eb2d467007bd7d81ad5c95ef5ef
masahirookubo <stmhamachiii@gmail.com>
2021-10-29 00:42:49 +0900

    prockfileの追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,13 @@
 class HomeController < ApplicationController
   def index
     @categories = [
-      { name: '音楽', description: "音楽療法\n和太鼓奏法など", image: '' }
+      { name: '音楽', description: "音楽療法\n和太鼓奏法など", image: 'https://recreation.sandbox.everyplus.jp/images/category/musuc.webp' },
+      { name: '健康', description: "音楽療法\n和太鼓奏法など", image: 'https://recreation.sandbox.everyplus.jp/images/category/musuc.webp' },
+      { name: '趣味', description: "音楽療法\n和太鼓奏法など", image: 'https://recreation.sandbox.everyplus.jp/images/category/musuc.webp' },
+      { name: '創作', description: "音楽療法\n和太鼓奏法など", image: 'https://recreation.sandbox.everyplus.jp/images/category/musuc.webp' },
+      { name: '旅行', description: "音楽療法\n和太鼓奏法など", image: 'https://recreation.sandbox.everyplus.jp/images/category/musuc.webp' },
+      { name: '飲食', description: "音楽療法\n和太鼓奏法など", image: 'https://recreation.sandbox.everyplus.jp/images/category/musuc.webp' },
+      { name: 'その他', description: "音楽療法\n和太鼓奏法など", image: 'https://recreation.sandbox.everyplus.jp/images/category/musuc.webp' },
     ]
   end
 end
