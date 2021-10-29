f9017a19f278fb69b257669a777b7b315587e8ab
masahirookubo <stmhamachiii@gmail.com>
2021-10-29 15:19:41 +0900

    detailの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
   get 'home/index'
+  get '/detail' => 'home#detail'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
