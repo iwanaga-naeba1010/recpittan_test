43de7b9a6d71b9bfc72e30fb7af840177d4b53aa
masahirookubo <stmhamachiii@gmail.com>
2021-11-03 01:11:18 +0900

    adminログアウトの実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,14 +9,14 @@ class ApplicationController < ActionController::Base
 
   # active adminをUserでログインするために必要
   # NOTE: best_practicesで引っかかるので一旦コメント
-  # def authenticate_admin!
-  #   if ENV['BASIC_AUTH']
-  #     user, password = ENV['BASIC_AUTH'].split(',').map(&:strip)
-  #     authenticate_or_request_with_http_basic('basic auth') do |input_user, input_pass|
-  #       input_user == user && Digest::SHA1.hexdigest(input_pass) == password
-  #     end
-  #   end
+  def authenticate_admin!
+    if ENV['BASIC_AUTH']
+      user, password = ENV['BASIC_AUTH'].split(',').map(&:strip)
+      authenticate_or_request_with_http_basic('basic auth') do |input_user, input_pass|
+        input_user == user && Digest::SHA1.hexdigest(input_pass) == password
+      end
+    end
 
-  #   redirect_to new_user_session_path unless current_user.role.admin?
-  # end
+    redirect_to new_user_session_path unless current_user.role.admin?
+  end
 end
