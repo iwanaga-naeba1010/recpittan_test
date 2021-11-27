bc172b92fe071db669b1ec6b6633f726816628b9
masahirookubo <stmhamachiii@gmail.com>
2021-11-27 20:26:35 +0900

    一覧の修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,21 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    @categories = Tag.categories
+    @categories = sort_categories(Tag.categories)
     @recs = Recreation.all
+  end
+
+  def sort_categories(categories)
+    categories = categories.to_a
+    [
+      categories.map {|c| c if c.name == '音楽'}.compact.first,
+      categories.map {|c| c if c.name == '健康'}.compact.first,
+      categories.map {|c| c if c.name == '趣味'}.compact.first,
+      categories.map {|c| c if c.name == '創作'}.compact.first,
+      categories.map {|c| c if c.name == '旅行'}.compact.first,
+      categories.map {|c| c if c.name == '飲食'}.compact.first,
+      categories.map {|c| c if c.name == 'イベント'}.compact.first,
+      categories.map {|c| c if c.name == 'その他'}.compact.first,
+    ]
   end
 end
