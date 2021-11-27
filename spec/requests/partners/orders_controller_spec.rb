e049db23116a0fe63e7f7f41fb5664d9b4eeb3d0
masahirookubo <stmhamachiii@gmail.com>
2021-11-27 22:47:15 +0900

    partnerのテスト修正

--- spec/requests/partners/orders_controller_spec.rb
+++ spec/requests/partners/orders_controller_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe Partners::OrdersController, type: :request do
 
     context 'with invalid user' do
       it 'return 302 when customer accessed' do
-        sign_out partner.user
+        sign_out partner
         sign_in customer
         get partners_order_path(order.id)
         expect(response).to have_http_status(:found)
@@ -29,7 +29,7 @@ RSpec.describe Partners::OrdersController, type: :request do
       end
 
       it 'return 302 when user not logged in' do
-        sign_out partner.user
+        sign_out partner
         get partners_order_path(order.id)
         expect(response).to have_http_status(:found)
         expect(response).to redirect_to new_user_session_path
@@ -55,7 +55,7 @@ RSpec.describe Partners::OrdersController, type: :request do
       end
 
       it 'return 302 when user not logged in' do
-        sign_out partner.user
+        sign_out partner
         get chat_partners_order_path(order.id)
         expect(response).to have_http_status(:found)
         expect(response).to redirect_to new_user_session_path
