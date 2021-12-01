0a591b30355d0e0336b08cb18273b169712163a7
masahirookubo <stmhamachiii@gmail.com>
2021-12-01 14:45:47 +0900

    redisの追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,15 +22,18 @@ Rails.application.configure do
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
 
-    config.cache_store = :memory_store
+    # config.cache_store = :memory_store
     config.public_file_server.headers = {
       'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
+    config.session_store :redis_store, servers: ENV['REDIS_URL'], expire_after: 1.day
+    # config.cache_store = :redis_store, ENV['REDIS_URL'], { expires_in: 90.minutes }
   else
     config.action_controller.perform_caching = false
 
     config.cache_store = :null_store
   end
+  # config.cache_store = :redis_store, ENV['REDIS_URL'], { expires_in: 90.minutes }
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
