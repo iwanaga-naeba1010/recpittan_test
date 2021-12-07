8ce2a5f0e0bd6a620cd02c665c7a35fbdbacff61
masahirookubo <stmhamachiii@gmail.com>
2021-12-07 19:21:29 +0900

    confirmの画面遷移追加

--- app/controllers/partners/orders_controller.rb
+++ app/controllers/partners/orders_controller.rb
@@ -16,8 +16,15 @@ class Partners::OrdersController < Partners::ApplicationController
   end
 
   def confirm
+    is_confirm = params[:is_confirm]
     @order = current_user.recreations.map do |rec|
       rec.orders.map { |order| order if order.id == params[:id].to_i }
     end.flatten.compact.first
+
+    if is_confirm == 'deny'
+      return render 'partners/orders/deny'
+    elsif is_confirm == 'accept'
+      return render 'partners/orders/accept'
+    end
   end
 end
