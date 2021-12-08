97763bec09d7bae37a68ab818109febc7db465a6
yanyu <dcjcsk0205@gmail.com>
2021-12-08 20:39:31 +0900

    仮push

--- app/models/order_tag.rb
+++ app/models/order_tag.rb
@@ -10,15 +10,8 @@
 #  order_id   :bigint           not null
 #  tag_id     :bigint           not null
 #
-# Indexes
-#
-#  index_order_tags_on_order_id  (order_id)
-#  index_order_tags_on_tag_id    (tag_id)
-#
 # Foreign Keys
 #
-#  fk_rails_...              (order_id => orders.id)
-#  fk_rails_...              (tag_id => tags.id)
 #  order_tags_order_id_fkey  (order_id => orders.id)
 #  order_tags_tag_id_fkey    (tag_id => tags.id)
 #
--- spec/models/plan_spec.rb
+++ spec/models/plan_spec.rb
@@ -10,13 +10,8 @@
 #  updated_at :datetime         not null
 #  company_id :bigint           not null
 #
-# Indexes
-#
-#  index_plans_on_company_id  (company_id)
-#
 # Foreign Keys
 #
-#  fk_rails_...           (company_id => companies.id)
 #  plans_company_id_fkey  (company_id => companies.id)
 #
 require 'rails_helper'
