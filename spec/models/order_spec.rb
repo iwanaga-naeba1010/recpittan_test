cadb9a7c2763ba0563b09b77aac13872379b849d
masahirookubo <stmhamachiii@gmail.com>
2021-12-10 16:40:21 +0900

    案件statusbのテスト追加

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -29,5 +29,35 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:partner) { create :user, :with_recreations }
+  let(:customer) { create :user, :with_custoemr }
+  let(:order) { create :order, recreation_id: partner.recreations.first.id, user_id: customer.id }
+  describe 'before_save::switch_status_befire_save' do
+    context 'with valid parameters' do
+      it 'changes nothing when status is finished' do
+        order.update(status: :finished)
+        expect(order.status).to eq :finished
+      end
+
+      it 'changes nothing when status is invoice_issued' do
+        order.update(status: :invoice_issued)
+        expect(order.status).to eq :invoice_issued
+      end
+
+      it 'changes nothing when status is paid' do
+        order.update(status: :paid)
+        expect(order.status).to eq :paid
+      end
+
+      it 'changes nothing when status is canceled' do
+        order.update(status: :canceled)
+        expect(order.status).to eq :canceled
+      end
+
+      it 'changes nothing when status is travled' do
+        order.update(status: :travled)
+        expect(order.status).to eq :travled
+      end
+    end
+  end
 end
