0ebd8f1f08ab70c6675867d80157b9ce348f84b1
masahirookubo <stmhamachiii@gmail.com>
2021-12-13 16:59:56 +0900

    customersにreport editの追加

---
+++ app/controllers/customers/reports_controller.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+class Customers::ReportsController < Customers::ApplicationController
+  before_action :set_report
+
+  def edit; end
+  def update
+    if @order.update(date_and_time: str_to_date)
+      redirect_to customers_order_path(@order.id), notice: '終了報告を更新しました'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_report
+    @report = current_user.orders.map do |order|
+      order if order&.report&.id == params[:id].to_i
+    end.compact&.first
+  end
+
+  def params_create
+    params.require(:report).permit(
+      :body, :expenses, :facility_count, :is_accepted,
+      :additional_number_of_people, :number_of_people, :transportation_expenses
+    )
+  end
+end
