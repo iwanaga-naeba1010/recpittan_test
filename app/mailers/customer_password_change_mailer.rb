725621c43fc2b243348cfa5acb04d7116a0dfd19
yanyu <dcjcsk0205@gmail.com>
2021-12-14 21:12:24 +0900

    password変更mailer作成

---
+++ app/mailers/customer_password_change_mailer.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CustomerPasswordChangeMailer < ApplicationMailer
+  def notify(user)
+    template = EmailTemplate.find_by(kind: 3)
+    @user_name = user.username
+    @email = user.email
+    @url = edit_user_password_url
+
+    mail from: 'info@everyplus.jp', to: @email, subject: template.title
+  end
+end
---
+++ app/mailers/partner_password_change_mailer.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class PartnerPasswordChangeMailer < ApplicationMailer
+  def notify(user)
+    template = EmailTemplate.find_by(kind: 11)
+    @user_name = user.username
+    @email = user.email
+    @url = edit_user_password_url
+
+    mail from: 'info@everyplus.jp', to: @email, subject: template.title
+  end
+end
