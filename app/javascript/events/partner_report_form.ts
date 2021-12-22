9c4509f77e6c603892d952c0cf6dcd70ca0adcbf
masahirookubo <stmhamachiii@gmail.com>
2021-12-22 23:16:22 +0900

    参加人数に関するイベントの追加

--- app/javascript/events/partner_report_form.ts
+++ app/javascript/events/partner_report_form.ts
@@ -5,13 +5,21 @@ import * as $ from 'jquery';
 
 (() => {
   document.addEventListener('turbolinks:load', () => {
+    // NOTE: 追加施設数に関するイベント
     $('#report_number_of_facilities').on('change', () => {
       const PRICE_PER_ADDIOTIONAL_FACILITY: number = Number($('#additionalFacilityFee').text());
-      console.log('chaged');
-      console.log(PRICE_PER_ADDIOTIONAL_FACILITY);
-
       const numberOfFacilities: number = Number($('#report_number_of_facilities').val());
       $('#totalNumberOfFacilityPrice').text((numberOfFacilities * PRICE_PER_ADDIOTIONAL_FACILITY).toLocaleString() + '円');
+    });
+
+    // NOTE 参加人数に関するイベント
+    $('#report_number_of_people').on('change', () => {
+      const PRICE_PER_ADDIOTIONAL_PERSON: number = Number($('#instructoMaterialAmount').text());
+      const numberOfPeople: number = Number($('#report_number_of_people').val());
+      // NOTE: 材料費総額
+      $('#totalMaterialPrice').text((numberOfPeople * PRICE_PER_ADDIOTIONAL_PERSON) + '円');
+      // NOTE: 追加人数
+      $('#numberOfPeople').text(numberOfPeople + '人');
     })
   });
 })();
