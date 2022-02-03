5ac29279a5437a2058a047e5a957c6de1c5ea949
masahirookubo <stmhamachiii@gmail.com>
2022-02-04 00:22:45 +0900

    アカウントページでzipをloadしても利用可能な状態に変更

--- app/javascript/events/edit_user_profile_form.ts
+++ app/javascript/events/edit_user_profile_form.ts
@@ -5,24 +5,32 @@ import * as $ from 'jquery';
 import { findAllPrefectures } from '../packs/prefectures';
 import { findAddressByZip } from '../packs/zip';
 
-(() => {
-  document.addEventListener('turbolinks:load', async () => {
-    // NOTE: 都道府県を初期化
-    const prefectures = await findAllPrefectures();
-    prefectures.result.map((prefecture) => {
-      $('#user_company_attributes_prefecture').append($('<option>', {
-        value: prefecture.prefName,
-        text: prefecture.prefName,
-      }));
-    });
-
-    // NOTE: user registration eidtの郵便番号をevent経由で動的set
-    $('#searchAddressWithZipInRegistrationEditForm').on('click', async () => {
-      const zip: string = $('#user_company_attributes_zip').val() as string;
-      const address = await findAddressByZip(zip);
-      $('#user_company_attributes_prefecture').val(address.results[0].address1);
-      $('#user_company_attributes_city').val(address.results[0].address2);
-      $('#user_company_attributes_street').val(address.results[0].address3);
-    });
+const App = async () => {
+  // NOTE: 都道府県を初期化
+  const prefectures = await findAllPrefectures();
+  prefectures.result.map((prefecture) => {
+    $('#user_company_attributes_prefecture').append($('<option>', {
+      value: prefecture.prefName,
+      text: prefecture.prefName,
+    }));
   });
-})();
+
+  // NOTE: user registration eidtの郵便番号をevent経由で動的set
+  $('#searchAddressWithZipInRegistrationEditForm').on('click', async () => {
+    const zip: string = $('#user_company_attributes_zip').val() as string;
+    const address = await findAddressByZip(zip);
+    $('#user_company_attributes_prefecture').val(address.results[0].address1);
+    $('#user_company_attributes_city').val(address.results[0].address2);
+    $('#user_company_attributes_street').val(address.results[0].address3);
+  });
+}
+
+// NOTE: 画面遷移した時用
+document.addEventListener("turbolinks:load", () => {
+  App();
+});
+
+// NOTE: リフレッシュした時用
+$(document).ready(() => {
+  App();
+});
