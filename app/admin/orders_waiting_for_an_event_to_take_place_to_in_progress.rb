17a63abcbddb731c517b5bac4c6576d7f8698168
masahirookubo <stmhamachiii@gmail.com>
2022-03-08 16:15:07 +0900

    実地待ちから相談中の強制執行モード追加

---
+++ app/admin/orders_waiting_for_an_event_to_take_place_to_in_progress.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+ActiveAdmin.register Orders::WaitingForAnEventToTakePlaceToInProgress do
+  actions :all, except: %i[show  destroy]
+  menu parent: '強制執行モード'
+
+  index do
+    id_column
+
+    column(:user) { |order| link_to order.user.company.facility_name, admin_company_path(order.user.company.id) }
+    column :recreation
+    column :start_at
+    actions
+  end
+
+  form do |f|
+    f.semantic_errors
+    f.actions
+  end
+
+  controller do
+    def update
+      order = Order.find(params[:id].to_i)
+      # NOTE(okubo): 本来statusは自動で切り替わるが、ここは本当に強制的なので注意してください
+      order.update!(start_at: nil, is_accepted: false, status: :in_progress)
+
+      SlackNotifier
+        .new(channel: '#アクティブチャットスレッド')
+        .send('管理画面から「実地待ち→相談中」の強制執行を行いました', "管理画面案件URL：#{admin_order_url(order.id)}")
+      redirect_to admin_order_path(order.id)
+    end
+  end
+end
---
+++ spec/requests/admin/orders_waiting_for_an_event_to_take_place_to_in_progress_controller_spec.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'OrdersWaitingForAnEventToTakePlaceToInProgress', type: :request do
+  let(:admin) { create :user, :with_admin }
+  let(:customer) { create :user, :with_custoemr }
+  let(:partner) { create :user, :with_recreations }
+  let!(:order) do
+    create(
+      :order,
+      recreation_id: partner.recreations.first.id,
+      user_id: customer.id,
+      # status: :waiting_for_an_event_to_take_place,
+      start_at: DateTime.tomorrow,
+      is_accepted: true
+    )
+  end
+
+  before do
+    sign_in admin
+  end
+
+  describe 'GET #index' do
+    it 'return http success' do
+      get admin_orders_waiting_for_an_event_to_take_place_to_in_progresses_path
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe 'GET #edit' do
+    it 'return http success' do
+      get edit_admin_orders_waiting_for_an_event_to_take_place_to_in_progress_path(order.id)
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe 'PUT #update' do
+    context 'when valid parameters' do
+      it 'returns 302 status' do
+
+        put admin_orders_waiting_for_an_event_to_take_place_to_in_progress_path(order.id), params: { orders_waiting_for_an_event_to_take_place_to_in_progress: {} }
+        expect(response).to have_http_status(:found)
+      end
+
+      it 'updates in_progress to waiting_for_an_event_to_take_place' do
+        expect {
+          put admin_orders_waiting_for_an_event_to_take_place_to_in_progress_path(order.id), params: { orders_waiting_for_an_event_to_take_place_to_in_progress: {} }
+          order.reload
+        }.to change { Order.find(order.id).status }.from('waiting_for_an_event_to_take_place').to('in_progress')
+      end
+
+      it 'updates is accepted' do
+        expect {
+          put admin_orders_waiting_for_an_event_to_take_place_to_in_progress_path(order.id), params: { orders_waiting_for_an_event_to_take_place_to_in_progress: {} }
+          order.reload
+        }.to change { Order.find(order.id).is_accepted }.from(true).to(false)
+      end
+    end
+  end
+end
