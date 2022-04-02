cc36618c083b8fa518f86bfd1d0405d030abbd10
masahirookubo <stmhamachiii@gmail.com>
2022-04-02 13:04:38 +0900

    formにカテゴリー追加

--- app/controllers/partners/recreations_controller.rb
+++ app/controllers/partners/recreations_controller.rb
@@ -2,6 +2,7 @@
 
 class Partners::RecreationsController < Partners::ApplicationController
   before_action :set_recreation, only: %i[show edit update]
+  before_action :set_tags, only: %i[new edit]
 
   def index
     @recreations = current_user.recreations
@@ -40,13 +41,21 @@ class Partners::RecreationsController < Partners::ApplicationController
     @recreation = current_user.recreations.find(params[:id])
   end
 
+  def set_tags
+    tags = Tag.all.to_a
+    @events = tags.map { |tag| tag if tag.kind == :tag }.compact
+    @categories = tags.map { |tag| tag if tag.kind == :category }.compact
+    @targets = tags.map { |tag| tag if tag.kind == :target }.compact
+  end
+
   def params_create
     params.require(:recreation).permit(
       :title, :second_title, :minutes, :description,
       :flow_of_day, :borrow_item, :bring_your_own_item, :extra_information, :youtube_id,
       :capacity, :instructor_amount, :instructor_material_amount,
       :instructor_name, :instructor_title, :instructor_description, :instructor_image,
-      :is_online, :is_public, :additional_facility_fee
+      :is_online, :is_public, :additional_facility_fee,
+      { tag_ids: [] }
     )
   end
 end
