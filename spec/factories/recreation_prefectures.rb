c958a0ec2a7a6005de217ba34f70481a82f587f9
masahirookubo <stmhamachiii@gmail.com>
2022-04-30 23:03:24 +0900

    dockerfileにlibrary追加

--- spec/factories/recreation_prefectures.rb
+++ spec/factories/recreation_prefectures.rb
@@ -1,5 +1,23 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: recreation_prefectures
+#
+#  id            :bigint           not null, primary key
+#  name          :string
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  recreation_id :bigint           not null
+#
+# Indexes
+#
+#  index_recreation_prefectures_on_recreation_id  (recreation_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (recreation_id => recreations.id)
+#
 FactoryBot.define do
   factory :recreation_prefecture do
     recreation
--- spec/factories/recreation_profiles.rb
+++ spec/factories/recreation_profiles.rb
@@ -1,5 +1,20 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: recreation_profiles
+#
+#  id            :bigint           not null, primary key
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  profile_id    :bigint           not null
+#  recreation_id :bigint           not null
+#
+# Foreign Keys
+#
+#  recreation_profiles_profile_id_fkey     (profile_id => profiles.id)
+#  recreation_profiles_recreation_id_fkey  (recreation_id => recreations.id)
+#
 FactoryBot.define do
   factory :recreation_profile do
     recreation
