c964daa750623f88fd83d15ab4770e5ee3ab0227
masahirookubo <stmhamachiii@gmail.com>
2022-05-02 18:35:32 +0900

    adminのテスト修正

--- spec/requests/admin/companies_controller_spec.rb
+++ spec/requests/admin/companies_controller_spec.rb
@@ -3,94 +3,55 @@
 require 'rails_helper'
 
 RSpec.describe 'Companies', type: :request do
-  let(:admin) { create :user, :with_admin }
-  let!(:customer) { create :user, :with_customer }
+  include_context 'with authenticated admin'
+  let!(:customer) { create(:user, :with_customer) }
   let!(:company) { customer.company }
 
-  before do
-    sign_in admin
+  describe 'GET /admin/companies' do
+    it_behaves_like 'an endpoint returns 2xx status'
   end
 
-  describe 'GET #index' do
-    it 'return http success' do
-      get admin_companies_path
-      expect(response).to have_http_status(:ok)
+  describe 'GET /admin/companies/:id' do
+    let!(:id) { company.id }
+    it_behaves_like 'an endpoint returns 2xx status'
+  end
+
+  describe 'GET /admin/companies/new' do
+    it_behaves_like 'an endpoint returns 2xx status'
+  end
+
+  describe 'GET /admin/companies/:id/edit' do
+    let!(:id) { company.id }
+    it_behaves_like 'an endpoint returns 2xx status'
+  end
+
+  describe 'POST /admin/companies' do
+    let(:params) do
+      { company: attributes_for(:company).merge({ user_attributes: attributes_for(:user) }) }
+    end
+    let(:expected_redirect_to) { %r{/admin/companies/\d+} }
+
+    it_behaves_like 'an endpoint redirects match'
+
+    context 'with invalid params' do
+      let(:params) { { company: {} } }
+
+      it_behaves_like 'an endpoint returns 2xx status'
     end
   end
 
-  describe 'GET #show' do
-    it 'return http success' do
-      get admin_company_path(company.id)
-      expect(response).to have_http_status(:ok)
-    end
+  describe 'PATCH /admin/companies/:id' do
+    let!(:id) { company.id }
+    let(:params) { attributes_for(:company) }
+    let(:expected_redirect_to) { %r{/admin/companies/[0-9]+} }
+
+    it_behaves_like 'an endpoint redirects match'
   end
 
-  describe 'POST #create' do
-    let(:attrs) { attributes_for(:company).merge(user_attributes: attributes_for(:user)) }
-
-    context 'with valid parameters' do
-      it 'return http success when user not logged in' do
-        post admin_companies_path, params: { company: attrs }
-        expect(response).to have_http_status(:found)
-        expect(response).to redirect_to(admin_company_path(Company.last.id))
-      end
-
-      it 'can create user_company and increase one company record' do
-        expect {
-          post admin_companies_path, params: { company: attrs }
-        }.to change(Company, :count).by(+1)
-      end
-
-      it 'can create user_company and increase one user record' do
-        expect {
-          post admin_companies_path, params: { company: attrs }
-        }.to change(User, :count).by(+1)
-      end
-
-      it 'can create user with customer role' do
-        post admin_companies_path, params: { company: attrs }
-        expect(User.last.role).to eq :customer
-      end
-    end
-
-    # TODO: 失敗パターンも実装
-    context 'with invalid parameters' do
-    end
-  end
-
-  describe 'GET #edit' do
-    it 'return http success' do
-      get edit_admin_company_path(company.id)
-      expect(response).to have_http_status(:ok)
-    end
-  end
-
-  describe 'PUT #update' do
-    context 'when valid parameters' do
-      name = 'update company name'
-      it 'returns 302 status' do
-        put admin_company_path(company.id), params: { company: { name: name } }
-        expect(response).to have_http_status(:found)
-      end
-
-      it 'update status' do
-        expect {
-          put admin_company_path(company.id), params: { company: { name: name } }
-        }.to change { Company.find(company.id).name }.from(company.name).to(name)
-      end
-    end
-  end
-
-  # describe 'DELETE #destroy' do
-  #   context 'success' do
-  #     it 'reduce one record' do
-  #       expect { delete admin_company_path(company.id) }.to change(Company, :count).by(-1)
-  #     end
-  #
-  #     it 'redirects to managers company billboards path' do
-  #       delete admin_company_path(company.id)
-  #       expect(response).to redirect_to admin_companies_path
-  #     end
-  #   end
+  # NOTE(okubo): deleteないのでコメントアウト
+  # describe 'DELETE /admin/companies/:id' do
+  #   let(:id) { company.id }
+  #   let(:expected_redirect_to) { admin_companies_path }
+  #   it_behaves_like 'an endpoint returns 3xx status'
   # end
 end
