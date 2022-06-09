b0d0281d84e140bffd03e0cd61a17ecb0c67ac19
masahirookubo <stmhamachiii@gmail.com>
2022-06-09 19:14:22 +0900

    customersに並び替えボタンの追加

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,6 +2,8 @@
 
 class CustomersController < Customers::ApplicationController
   def index
-    @orders = current_user.orders
+    order_by = :start_at
+    order_by = params[:order] if params[:order].present?
+    @orders = current_user.orders.order("#{order_by} asc")
   end
 end
