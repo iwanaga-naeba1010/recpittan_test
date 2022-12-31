0e029de72ecb0ebe338f216ccfa5588c77d58641
yasanoyou <wearejanne2007@icloud.com>
2022-12-31 18:21:26 +0900

    依頼時の年選択から2022を削除
    fix spec
    
    fix id

--- app/models/concerns/year.rb
+++ app/models/concerns/year.rb
@@ -3,7 +3,6 @@
 class Year < ActiveHash::Base
   include ActiveHash::Associations
   self.data = [
-    { id: 1, value: '2022' },
-    { id: 2, value: '2023' }
+    { id: 1, value: '2023' }
   ]
 end
