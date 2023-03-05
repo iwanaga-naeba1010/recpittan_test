e3f080ba599cad73d7f4c6327621b7d1b98adfc1
yasanoyou <wearejanne2007@icloud.com>
2023-03-05 23:52:47 +0900

    fix lint

--- reactSrc/components/shared/form/forPartner/recreationNew/step.tsx
+++ reactSrc/components/shared/form/forPartner/recreationNew/step.tsx
@@ -6,18 +6,16 @@ type Props = {
   activeStep: number;
 };
 
-export const Step: React.FC<Props> = (props) => {
-  const { totalCounts, activeStep } = props;
-  console.log('activeStep', activeStep);
+export const Step: React.FC<Props> = ({ totalCounts, activeStep }) => {
   return (
     <div className='d-flex'>
-      {[...Array<number>(totalCounts)].map((_, step: number) =>
+      {[...Array(totalCounts)].map((_, step) =>
         step === activeStep ? (
           <p key={step} className='px-1 small text-black font-weight-bold border border-2 border-dark rounded-pill'>
             ステップ{step + 1}
           </p>
         ) : (
-          <StepItem currentStep={step + 1} />
+          <StepItem currentStep={step + 1} key={step} />
         )
       )}
     </div>
