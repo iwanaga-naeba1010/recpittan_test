8ea1b4205f6cf85ad12cdef343b49bfb6cbc82e4
yasanoyou <59010435+yasanoyou@users.noreply.github.com>
2023-08-30 22:04:31 +0900

    Revert "都道府県からレクリエーションを検索"

--- app/controllers/customers/recreations_controller.rb
+++ app/controllers/customers/recreations_controller.rb
@@ -4,13 +4,10 @@ class Customers::RecreationsController < Customers::ApplicationController
   skip_before_action :authenticate_user!
   skip_before_action :require_customer
 
-  # rubocop:disable Metrics/AbcSize
   def index
-    @q = Recreation.includes(:recreation_images, :recreation_prefectures).public_recs.ransack(params[:q])
+    @q = Recreation.includes(:recreation_images).public_recs.ransack(params[:q])
     @categories = Tag.categories
     @recs = @q.result(distinct: true).page(params[:page]).per(30)
-    @distinct_prefectures = RecreationPrefecture.select(:name).distinct.map(&:name)
-    @sorted_prefectures = RecreationPrefecture.names & @distinct_prefectures
     value = @q.base.conditions&.first&.values&.first&.value
     is_tag_class = @q.base.conditions&.first&.attributes&.first&.klass == Tag
     if value && is_tag_class
@@ -18,7 +15,6 @@ class Customers::RecreationsController < Customers::ApplicationController
       @tag = value.is_a?(Integer) ? Tag.find(value.to_i) : Tag.find_by(name: value)
     end
   end
-  # rubocop:enable Metrics/AbcSize
 
   def show
     @recreation = Recreation.public_recs.includes(:orders).find(params[:id])
