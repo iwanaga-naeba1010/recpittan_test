e5548754ea94c19fdaca18ec491e4c131a6b0f54
masahirookubo <stmhamachiii@gmail.com>
2024-04-21 16:31:06 +0900

    feat: registration controllerにrefaptcha追加

--- app/controllers/custom_devise/registrations_controller.rb
+++ app/controllers/custom_devise/registrations_controller.rb
@@ -2,9 +2,24 @@
 
 # rubocop:disable Rails/LexicallyScopedActionFilter
 class CustomDevise::RegistrationsController < Devise::RegistrationsController
+  include Recaptcha::Adapters::ControllerMethods
   include Recaptcha::Adapters::ViewMethods
   before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
+  prepend_before_action :check_captcha, only: [:create]
+
+  private def check_captcha
+    return if verify_recaptcha
+
+    self.resource = resource_class.new sign_up_params
+    resource.validate
+    set_minimum_password_length
+
+    respond_with_navigational(resource) do
+      flash.discard(:recaptcha_error)
+      render :new
+    end
+  end
 
   # GET /resource/sign_up
   def new
