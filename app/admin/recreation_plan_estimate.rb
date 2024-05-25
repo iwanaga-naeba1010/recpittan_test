c2b00bf45d4a456debad798b9e9062420d9f2fe4
yasanoyou <wearejanne2007@icloud.com>
2024-05-25 22:19:48 +0900

    lint

--- app/admin/recreation_plan_estimate.rb
+++ app/admin/recreation_plan_estimate.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-# rubocop:disable Metrics/BlockLength
 ActiveAdmin.register RecreationPlanEstimate do
   actions :index, :show
 end
-# rubocop:enable Metrics/BlockLength
