f9e6533d4af956825065d038ce73254647544e19
yasanoyou <wearejanne2007@icloud.com>
2024-05-25 22:14:16 +0900

    見積書の管理画面の設定

---
+++ app/admin/recreation_plan_estimate.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+# rubocop:disable Metrics/BlockLength
+ActiveAdmin.register RecreationPlanEstimate do
+  actions :index, :show
+end
+# rubocop:enable Metrics/BlockLength
