fea9b80d7ed91c351c2479eb0b80e88087427501
yasanoyou <wearejanne2007@icloud.com>
2024-07-07 15:17:30 +0900

    プランをadminも見れるように

--- app/controllers/api_customer/recreation_plans_controller.rb
+++ app/controllers/api_customer/recreation_plans_controller.rb
@@ -2,6 +2,9 @@
 
 module ApiCustomer
   class RecreationPlansController < ApplicationController
+    skip_before_action :require_customer, only: [:show]
+    before_action :require_customer_or_admin
+
     def show
       recreation_plan = RecreationPlan.includes(
         recreation_recreation_plans: [
@@ -11,6 +14,14 @@ module ApiCustomer
         ]
       ).find(params[:id])
       render json: RecreationPlanSerializer.new.serialize(recreation_plan:)
+    end
+
+    private
+
+    def require_customer_or_admin
+      return if current_user.role.customer? || current_user.role.admin?
+
+      render_json(['権限がありません'], status: 401)
     end
   end
 end
