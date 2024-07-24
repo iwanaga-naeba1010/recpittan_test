f0f88d114fecf9a0c5b996a9bb31ded414c0aa52
yasanoyou <wearejanne2007@icloud.com>
2024-07-24 22:58:52 +0900

    口座情報フォームの修正

---
+++ reactSrc/utils/bank.ts
@@ -0,0 +1,54 @@
+import axios from 'axios';
+import { Bank, Branch } from '@/types';
+
+export const fetchAllBanks = async (): Promise<Bank[]> => {
+  let fetchedBanks: Bank[] = [];
+  let currentPage = 1;
+
+  while (currentPage <= 24) {
+    const api = `https://bank.teraren.com/banks.json?page=${currentPage}`;
+    const response = await axios.get(api);
+    const data = response.data;
+
+    if (data && data.length > 0) {
+      fetchedBanks = fetchedBanks.concat(data);
+      currentPage += 1;
+    } else {
+      break;
+    }
+  }
+  return fetchedBanks;
+};
+
+export const fetchAllBranches = async (bankCode: string): Promise<Branch[]> => {
+  let allBranches: Branch[] = [];
+  let currentPage = 1;
+  let hasMore = true;
+
+  while (hasMore) {
+    const api = `https://bank.teraren.com/banks/${bankCode}/branches.json?page=${currentPage}`;
+    const response = await axios.get(api);
+    const data = response.data;
+
+    if (data && data.length > 0) {
+      allBranches = allBranches.concat(data);
+      currentPage++;
+    } else {
+      hasMore = false;
+    }
+  }
+  return allBranches;
+};
+
+export const fetchBranchByCode = async (
+  bankCode: string,
+  branchCode: string
+): Promise<Branch | null> => {
+  const api = `https://bank.teraren.com/banks/${bankCode}/branches/${branchCode}.json`;
+  try {
+    const response = await axios.get(api);
+    return response.data;
+  } catch (error) {
+    return null;
+  }
+};
