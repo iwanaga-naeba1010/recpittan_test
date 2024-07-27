a9d0c16fdf59f4210e5d3b1ad7979d5e5eeab25b
yasanoyou <wearejanne2007@icloud.com>
2024-07-27 14:13:17 +0900

    fix controller

--- app/controllers/partners/registrations_controller.rb
+++ app/controllers/partners/registrations_controller.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class Partners::RegistrationsController < Partners::ApplicationController
-  skip_before_action :authenticate_user!, only: %i[new]
-  skip_before_action :require_partner, only: %i[new]
+  skip_before_action :authenticate_user!, only: %i[new complete confirm]
+  skip_before_action :require_partner, only: %i[new complete confirm]
   before_action :redirect_if_partner_logged_in, only: %i[new]
   layout 'partner_registration'
 
