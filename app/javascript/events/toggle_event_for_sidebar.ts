3d39277563a4839040f2f35225e85cd8bfd82c3e
yasanoyou <wearejanne2007@icloud.com>
2024-07-29 23:54:40 +0900

    パートナーページの各headerを新しいものへ変更
    不要なcssを削除
    
    fix header

--- app/javascript/events/toggle_event_for_sidebar.ts
+++ app/javascript/events/toggle_event_for_sidebar.ts
@@ -3,40 +3,16 @@ import * as $ from 'jquery';
 (() => {
   document.addEventListener('turbolinks:load', () => {
     $('#hamburger').on('click', () => {
-      const line1 = document.getElementById('line1');
-      const line2 = document.getElementById('line2');
-      const line3 = document.getElementById('line3');
       const nav = document.getElementById('nav');
 
-      if (line1) {
-        line1.classList.toggle('line_top');
-      }
-      if (line2) {
-        line2.classList.toggle('line_middle');
-      }
-      if (line3) {
-        line3.classList.toggle('line_bottom');
-      }
       if (nav) {
         nav.classList.toggle('fade-in');
       }
     });
 
     $('#close-sidebar').on('click', () => {
-      const line1 = document.getElementById('line1');
-      const line2 = document.getElementById('line2');
-      const line3 = document.getElementById('line3');
       const nav = document.getElementById('nav');
 
-      if (line1) {
-        line1.classList.remove('line_top');
-      }
-      if (line2) {
-        line2.classList.remove('line_middle');
-      }
-      if (line3) {
-        line3.classList.remove('line_bottom');
-      }
       if (nav) {
         nav.classList.remove('fade-in');
       }
