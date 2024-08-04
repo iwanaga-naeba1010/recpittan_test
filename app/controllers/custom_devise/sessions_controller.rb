f3ec36ca31b152915b338f80be1ed0a35a257abc
yasanoyou <wearejanne2007@icloud.com>
2024-08-04 21:33:50 +0900

    fix redirect

--- app/controllers/custom_devise/sessions_controller.rb
+++ app/controllers/custom_devise/sessions_controller.rb
@@ -1,4 +1,3 @@
-# app/controllers/custom_devise/sessions_controller.rb
 # frozen_string_literal: true
 
 class CustomDevise::SessionsController < Devise::SessionsController
@@ -23,10 +22,15 @@ class CustomDevise::SessionsController < Devise::SessionsController
 
   def after_sign_in_path_for(resource)
     stored_location = stored_location_for(resource)
-    return admin_dashboard_path if resource.role.admin? && (stored_location.nil? || stored_location == root_path)
-    return partners_path(is_open: true) if resource.role.partner? && (stored_location.nil? || stored_location == root_path)
+    return stored_location unless stored_location.nil? || stored_location == root_path
 
-    stored_location || session[:redirect_url] || customers_path
+    if resource.role.admin?
+      admin_dashboard_path
+    elsif resource.role.partner?
+      partners_path(is_open: true)
+    else
+      session[:redirect_url] || customers_path
+    end
   end
 
   # If you have extra params to permit, append them to the sanitizer.
