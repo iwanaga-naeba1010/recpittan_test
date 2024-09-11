0c745caea2a549c099a827dc768256c0233b93a4
yasanoyou <wearejanne2007@icloud.com>
2024-09-11 22:23:35 +0900

    fix preview image

--- app/javascript/events/partner_profile_form.ts
+++ app/javascript/events/partner_profile_form.ts
@@ -3,24 +3,34 @@
  */
 import * as $ from 'jquery';
 
-const App = (inputSelector: string, displaySelector: string) => {
-  $(`#${inputSelector}`).on('input', () => {
-    const currentValue =
-      ($(`#${inputSelector}`).val() ?? '').toString().length ?? 0;
-    $(`#${displaySelector}`).text(currentValue);
+$(document).on('turbolinks:load', function() {
+  const imageField = $('.image_form_contents_field');
+  const previewContent = $('.prev-content');
+  const existingImage = $('.prev-image');
+
+  imageField.on('change', (event) => {
+    const file = (event.target as HTMLInputElement).files?.[0];
+    
+    if (!file) return;
+
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      if (!e.target?.result) return;
+
+      const imageUrl = e.target.result.toString();
+      if (existingImage.length === 0) {
+        // 新しい画像要素を作成してプレビュー表示
+        const newImage = $('<img>', {
+          src: imageUrl,
+          class: 'prev-image'
+        });
+        previewContent.empty().append(newImage);
+      } else {
+        // 既存の画像要素に画像を設定
+        existingImage.attr('src', imageUrl);
+      }
+    };
+    reader.readAsDataURL(file);
   });
-};
-
-// NOTE: 画面遷移した時用
-document.addEventListener('turbolinks:load', () => {
-  App('profile_name', 'countOfProfileName');
-  App('profile_title', 'countOfProfileTitle');
-  App('profile_description', 'countOfProfileDescription');
 });
 
-// NOTE: リフレッシュした時用
-$(document).ready(() => {
-  App('profile_name', 'countOfProfileName');
-  App('profile_title', 'countOfProfileTitle');
-  App('profile_description', 'countOfProfileDescription');
-});
