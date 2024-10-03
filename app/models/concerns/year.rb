f42e4291656fcb93284e15a37e4f20319dedde00
yasanoyou <wearejanne2007@icloud.com>
2024-10-03 17:04:49 +0900

    add year

--- app/models/concerns/year.rb
+++ app/models/concerns/year.rb
@@ -3,6 +3,7 @@
 class Year < ActiveHash::Base
   include ActiveHash::Associations
   self.data = [
-    { id: 1, value: '2024' }
+    { id: 1, value: '2024' },
+    { id: 2, value: '2025' }
   ]
 end
