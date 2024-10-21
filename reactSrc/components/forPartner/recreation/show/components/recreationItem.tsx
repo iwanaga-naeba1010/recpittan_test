8e7b7f0f9f40983837161c1c7b2abf8fad1ac7ab
yasanoyou <wearejanne2007@icloud.com>
2024-10-21 22:53:26 +0900

    パートナーのレクページでtitleの重複表示を修正

--- reactSrc/components/forPartner/recreation/show/components/recreationItem.tsx
+++ reactSrc/components/forPartner/recreation/show/components/recreationItem.tsx
@@ -2,18 +2,16 @@ import React from 'react';
 
 type Props = {
   title: string;
-  content: string;
   url: string;
 };
 
 export const RecreationItem: React.FC<Props> = (props) => {
-  const { title, content, url } = props;
+  const { title, url } = props;
   return (
     <a href={url}>
       <div className='d-flex bd-highlight border-bottom'>
         <div className='py-2 w-100 bd-highlight'>
           <div>{title}</div>
-          <div>{content}</div>
         </div>
         <div className='py-2 flex-shrink-1 bd-highlight'>
           <i className='fas fa-chevron-right'></i>
