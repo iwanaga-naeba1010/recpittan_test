13a4889033052f193a25ba9bf76c816990639700
yasanoyou <wearejanne2007@icloud.com>
2024-12-03 01:07:37 +0900

    fix config

--- config/application.rb
+++ config/application.rb
@@ -10,9 +10,6 @@ require 'active_support/parameter_filter'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-# NOTE(okubo): DEPRECATION WARNINGを出さないようにする
-ActiveSupport::Deprecation.silenced = true
-
 module MachingSystem
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
