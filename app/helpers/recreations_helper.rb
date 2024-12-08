4ffbcb21c16056282372c7195a51e397ab1ae603
yasanoyou <wearejanne2007@icloud.com>
2024-12-08 17:43:05 +0900

    fix sort_order

--- app/helpers/recreations_helper.rb
+++ app/helpers/recreations_helper.rb
@@ -67,7 +67,7 @@ module RecreationsHelper
   end
 
   def sort_order_options
-    Recreation.sort_orders.map do |key, _|
+    Recreation.sort_order.values do |key, _|
       [I18n.t("recreation.sort_orders.#{key}"), key.to_s]
     end
   end
