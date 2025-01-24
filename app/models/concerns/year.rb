dd1e86f8436c287e2d1efe7bb001d0644d0a82b5
yasanoyou <wearejanne2007@icloud.com>
2025-01-24 22:24:54 +0900

    yaerに2026を追加

--- app/models/concerns/year.rb
+++ app/models/concerns/year.rb
@@ -3,7 +3,7 @@
 class Year < ActiveHash::Base
   include ActiveHash::Associations
   self.data = [
-    { id: 1, value: '2024' },
-    { id: 2, value: '2025' }
+    { id: 1, value: '2025' },
+    { id: 2, value: '2026' }
   ]
 end
