3b1b39a135077440dd4034842a77ab6c99e23997
yasanoyou <wearejanne2007@icloud.com>
2025-01-25 13:54:04 +0900

    fix year data

--- app/models/concerns/year.rb
+++ app/models/concerns/year.rb
@@ -3,7 +3,7 @@
 class Year < ActiveHash::Base
   include ActiveHash::Associations
 
-  current_year = Time.zone.today.year
+  current_year = Time.current.year
   self.data =  [
     { id: 1, value: current_year.to_s },
     { id: 2, value: (current_year + 1).to_s }
