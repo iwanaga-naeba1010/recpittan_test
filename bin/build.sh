fdcc75276dd0148e7a94ee17250b25c6dec2f75b
iwanaga <iwanaga@everyplus.jp>
2025-03-12 15:15:51 +0000

    adjust source

---
+++ bin/build.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+apk add --no-cache  gcc g++ libc-dev libxml2-dev linux-headers make postgresql-dev
+apk add yarn graphviz chromium-chromedriver git openssh
+
+
+mkdir -p vendor/bundle
+mkdir -p vendor/cache
+mkdir -p config/certs
+mkdir -p tmp/pids
+mkdir -p node_modules
+touch config/certs/server.crt
+touch config/certs/server.key
+
+chown -R app:app ./*
+chmod 600 config/certs/*
+chmod 777 /usr/local/lib/ruby
+chmod 777 /tmp
+
+# do on Dockerfile , it need root
+#gem install bundler --version 2.5.23
+echo 'gem: --no-document' > /usr/local/etc/gemrc
+gem install bundler --version 2.5.23
+
+
+cd /app
+
+su -c 'bin/bundle config set path /app/vendor/bundle' app
+# TODO productionの場合、
+#bin/bundle install --without development test --jobs 4
+
+echo ""
+echo "bundle install"
+su -c 'bin/bundle install --jobs 4' app
+
+echo ""
+echo "yarn install"
+#yarn install
+su -c 'yarn install' app
+
+echo ""
+echo "webpack compile"
+#su -c "bin/rails  webpacker:compile" app
+su -c "bin/webpack -d & " app
+
+
+
+
+echo ""
+echo "assets:precompile"
+su -c 'bin/rails assets:precompile' app
+
+echo ""
+echo "db:migrate"
+su -c 'bin/rails db:migrate' app
+
+echo ""
+echo "db:seed"
+su -c 'bin/rails db:seed' app
+
+
+
+rm -f tmp/pids/server.pid
+
---
+++ bin/clear.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+rm -fr erd.pdf
+
+
+echo "" > log/bullet.log
+echo "" > log/development.log
+rm -fr public/assets/*
+rm -fr public/packs/*
+rm -fr tmp/local_secret.txt
+rm -fr tmp/miniprofiler/*
+
