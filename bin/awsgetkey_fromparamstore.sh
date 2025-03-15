366f4930a508a5908959d0f3cd972b7a858cd454
iwanaga <iwanaga@everyplus.jp>
2025-03-15 21:28:38 +0000

    flowtest

--- bin/awsgetkey_fromparamstore.sh
+++ bin/awsgetkey_fromparamstore.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 #set -e  # エラーが発生した場合にスクリプトを即終了
 
+touch .env
+
 # set aws credeintials
 apk add aws-cli sudo
 
-#ak=$( grep AWS_ACCESS .env | cut -d '=' -f 2 | cut -d "'" -f 2)
-#ac=$( grep AWS_SECRET .env | cut -d '=' -f 2 | cut -d "'" -f 2)
-#echo $ak
-#echo $ac
+env | grep AWS_ACCESS > /dev/null
+ret=$?
+if [ $ret -eq 0 ]; then
+    ak=$AWS_ACCESS_KEY_ID
+    ac=$AWS_SECRET_ACCESS_KEY
+else
+    ak=$( grep AWS_ACCESS .env | cut -d '=' -f 2 | cut -d "'" -f 2)
+    ac=$( grep AWS_SECRET .env | cut -d '=' -f 2 | cut -d "'" -f 2)
+fi
 
-#cd
-#aws configure set aws_access_key_id $ak
-#aws configure set aws_secret_access_key $ac
-#aws configure set region ap-northeast-1
-#aws configure set output text
+
+cd
+aws configure set aws_access_key_id $ak
+aws configure set aws_secret_access_key $ac
+aws configure set region ap-northeast-1
+aws configure set output text
 
 cd /app
 # 証明書を保存するディレクトリを作成※ここはアプリに合わせて変更してください
@@ -34,19 +42,17 @@ chmod 600 "$CERT_DIR/server.key"
 
 echo "SSL certificate setup completed successfully."
 
-
-grep SLACK_WEBHOOK .env >> /dev/null
+env | grep SLACK_WEBHOOK > /dev/null
 ret=$?
-if [ $ret -eq 1 ]; then
+if [ $ret -ne 0 ];
     echo "get slack webhook"    
     slackwebhook=$( aws ssm get-parameter --with-decryption --query "Parameter.Value" --output text --name "SLACK_WEBHOOK" )
     echo "SLACK_WEBHOOK=${slackwebhook}" >> .env
 fi
 
-
-grep DATABASE_P .env >> /dev/null
+env | grep DATABASE_P > /dev/null
 ret=$?
-if [ $ret -eq 1 ]; then
+if [ $ret -ne 0 ]; then
     echo "get db"
     dbpass=$( aws ssm get-parameter --with-decryption --query "Parameter.Value" --output text --name "DATABASE_PASSWORD" )
     echo "DATABASE_PASSWORD=${dbpass}" >> .env
