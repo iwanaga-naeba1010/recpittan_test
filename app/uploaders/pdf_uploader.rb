cc874d9423849b1b2ef52a3e22b1fed9fff75722
yasanoyou <wearejanne2007@icloud.com>
2025-03-16 15:30:36 +0900

    fix PdfUploader

--- app/uploaders/pdf_uploader.rb
+++ app/uploaders/pdf_uploader.rb
@@ -1,16 +1,6 @@
 # frozen_string_literal: true
 
-class PdfUploader < CarrierWave::Uploader::Base
-  if Rails.env.local?
-    storage :file
-  else
-    storage :fog
-  end
-
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
-
+class PdfUploader < ImageUploader
   def extension_allowlist
     %w[pdf]
   end
